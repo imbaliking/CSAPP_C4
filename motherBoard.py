317573c63b5e0dfbc550d5218c86f5203a28c2fd
imbaliking <imbaliking@163.com>
2022-04-03 17:15:58 +0800

    新增中间状态打印功能，分阶段打印，以回车为暂停

--- motherBoard.py
+++ motherBoard.py
@@ -18,6 +18,10 @@ class mother_board(object):
         self.timer_comp = {}
         for timer_class in self.timer_comp_list:
             self.timer_comp[timer_class.__str__] = timer_class()
+        # 所有线路的名称，方便展示
+        self.mother_line = ["PC","icode","ifun","rA","rB","valC","valP","valA","valB",'dstE',"dstM","srcA","srcB","valE",
+                            "Cnd","valM","Stat","instr_valid","imem_error","dmem_error","need_valC","need_regids","set_cc",
+                            "aluA","aluB","alufun","ZF","OF","SF","mem_read","mem_write","mem_addr","mem_data","new_pc"]
 
         # 线路实例化成属性，储存当前值，方便查看
         self.PC = ""
@@ -40,7 +44,37 @@ class mother_board(object):
         self.instr_valid = False
         self.imem_error = False
         self.dmem_error = False
+        # 存储变化用的
+        self.last_data = {}
 
+    def display(self):
+        dirty_value = []
+        clean_value = []
+        for attr in dir(self):
+            if attr not in self.mother_line:
+                continue
+            if attr not in self.last_data:
+                self.last_data[attr] = getattr(self,attr)
+                dirty_value.append((attr,getattr(self,attr)))
+            elif self.last_data[attr] != getattr(self,attr):
+                dirty_value.append((attr,getattr(self,attr)))
+            else:
+                clean_value.append((attr,getattr(self,attr)))
+
+        dirty = ""
+        clean = ""
+        for attr_name,attr_value in dirty_value:
+            dirty += "%s:%s"%(attr_name,attr_value)
+        for attr_name,attr_value in clean_value:
+            clean += "%s:%s"%(attr_name,attr_value)
+
+        print "变化的值如下："
+        print dirty
+        print "没有变化的值如下"
+        print clean
+
+    def pause(self):
+        a = raw_input()
 
     def run(self):
         """
@@ -78,6 +112,10 @@ class mother_board(object):
             self.logic_comp["PC_Increment"].input_signal(need_valC = self.need_valC,need_regids = self.need_regids,PC=self.PC)
             self.valP = self.logic_comp["PC_Increment"].output_signal()["valP"]
 
+            print "阶段1："
+            self.display()
+            self.pause()
+
             # 第二阶段 译码
             # 写回是发生在下降沿的时刻，现在阶段是上升沿
             self.logic_comp["dstE"].input_signal(Cnd=self.Cnd,icode=self.icode,rB=self.rB)
@@ -93,6 +131,10 @@ class mother_board(object):
             self.timer_comp["registerFile"].switch_state()
             self.valA = self.timer_comp["registerFile"].output_signal()["valA"]
             self.valB = self.timer_comp["registerFile"].output_signal()["valB"]
+
+            print "阶段2："
+            self.display()
+            self.pause()
 
             # 第三阶段 执行
             self.logic_comp["SetCC"].input_signal(icode=self.icode)
@@ -116,6 +158,10 @@ class mother_board(object):
             self.logic_comp["Cond"].input_signal(ZF=self.ZF,OF=self.OF,SF=self.SF,ifun=self.ifun)
             self.Cnd = self.logic_comp["Cond"].output_signal()["Cnd"]
 
+            print "阶段3："
+            self.display()
+            self.pause()
+
             # 第四阶段 访存
             self.logic_comp["memRead"].input_signal(icode=self.icode)
             self.mem_read = self.logic_comp["memRead"].output_signal()["mem_read"]
@@ -132,6 +178,10 @@ class mother_board(object):
             self.dmem_error = self.timer_comp["dataMemory"].output_singal()["dmem_error"]
             self.logic_comp["Stat"].input_singal(imem_error=self.imem_error,dmem_error=self.dmem_error,icode=self.icode,instr_valid=self.instr_valid)
             self.Stat = self.logic_comp["Stat"].output_signal()["Stat"]
+
+            print "阶段4："
+            self.display()
+            self.pause()
 
             # 第五阶段 写回
             # 时钟切换到下降沿
@@ -155,6 +205,10 @@ class mother_board(object):
             self.timer_comp["programMemory"].switch_state()
             self.timer_comp["statusRegister"].switch_state()
 
+            print "阶段5："
+            self.display()
+            self.pause()
+
             # 第六阶段 更新PC
             self.logic_comp["newPC"].input_singal(icode=self.icode,Cnd=self.Cnd,valC=self.valC,valM=self.valM,valP=self.valP)
             self.new_pc = self.logic_comp["newPC"].output_singal()["new_pc"]
@@ -162,6 +216,9 @@ class mother_board(object):
             self.timer_comp["programCounter"].input_signal(PC=self.new_pc)
             self.timer_comp["programCounter"].switch_state()
 
+            print "阶段6："
+            self.display()
+            self.pause()
 
     def load_data(self,start_address,program_str):
         self.timer_comp["programCounter"].init_PC(start_address)
